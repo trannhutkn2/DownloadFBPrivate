920a1cfcaa04538e164bc8b35b2e003582a9341a
iamnga <51619515+iamnga@users.noreply.github.com>
2020-10-05 22:36:53 +0700

    Commit

---
+++ DownloadFBPrivate/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DownloadFBPrivate
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(255, 168);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(98, 77);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(403, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(98, 124);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(403, 20);
+            this.txtLink.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Link";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "From Mmo Underground With Love";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 0;
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 257);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "mainFrm";
+            this.Text = "Download Video FB In Private Group";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DownloadFBPrivate/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadFBPrivate
+{
+    public partial class mainFrm : Form
+    {
+        public mainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string fileName = txtName.Text;
+            string link = txtLink.Text;
+            ProcessStartInfo videoProcInfo = new ProcessStartInfo();
+            videoProcInfo.FileName = @"cmd.exe";
+            videoProcInfo.Arguments = $"/k youtube-dl \"{link}\" --cookies cookies.txt -f bestvideo --output \"video.%(ext)s\"&youtube-dl \"{link}\" --cookies cookies.txt -f bestaudio --output \"audio.%(ext)s\"&ffmpeg -i video.mp4 -i audio.m4a -c copy Output\\{fileName}.mp4&del \"video.mp4\"&del \"audio.m4a\"&echo ##### Successfully.......... #####";
+            Process videoProc = new Process();
+            videoProc.StartInfo = videoProcInfo;
+            videoProc.Start();
+        }
+    }
+}
